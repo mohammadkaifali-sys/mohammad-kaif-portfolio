08917ec7a0dcf8ab8367d577a4fe809057d01757
Mohammad Kaif Ali <mohammad.kaif.ali@jbitdoon.edu.in>
2026-07-09 22:54:13 +0530

    Fix Vercel build

---
+++ .next/server/chunks/_next-internal_server_app_resume_route_actions_1invi59.js
@@ -0,0 +1,3 @@
+module.exports=[74537,(e,o,d)=>{}];
+
+//# sourceMappingURL=_next-internal_server_app_resume_route_actions_1invi59.js.map
--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"u4g8tOKKF5aFJKQblg3Js3erYWg+PCNd7iimmg6cfiE=\"\n}"
